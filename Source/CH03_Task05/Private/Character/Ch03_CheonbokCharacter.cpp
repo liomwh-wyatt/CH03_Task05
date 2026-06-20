6874a0f5e5e9a9606feeb8175aca9f6267f5262a
liomwh-wyatt <liomwh@gmail.com>
2026-06-21 01:17:52 +0900

    천복이어셋제작

---
+++ Source/CH03_Task05/Private/Character/Ch03_CheonbokCharacter.cpp
@@ -0,0 +1,9 @@
+#include "Character/Ch03_CheonbokCharacter.h"
+
+Ch03_CheonbokCharacter::Ch03_CheonbokCharacter()
+{
+}
+
+Ch03_CheonbokCharacter::~Ch03_CheonbokCharacter()
+{
+}
---
+++ Source/CH03_Task05/Public/Character/Ch03_CheonbokCharacter.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+
+class CH03_TASK05_API Ch03_CheonbokCharacter
+{
+public:
+	Ch03_CheonbokCharacter();
+	~Ch03_CheonbokCharacter();
+};
